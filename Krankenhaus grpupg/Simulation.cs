6bb9a1cdfa34e7aadea3d1b5c948eb124f800acd
Robin <Joooowy@gmail.com>
2021-03-08 13:24:00 +0100

    Ny exe fil project

---
+++ Krankenhaus grpupg/Simulation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalLibrary;
+
+namespace Krankenhaus_grpupg
+{
+    class Simulation
+    {
+        public Hospital MyProperty { get; set; }
+    }
+}
